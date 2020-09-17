c4dd57bfc251586e6dd74ecfacdffef86d196dca
Crew Spence <csspence123@gmail.com>
2020-09-17 05:54:25 -0500

    Created Node and DoublyLinkedList class constructors.

--- doubly_linked_list_udemy.js
+++ doubly_linked_list_udemy.js
@@ -3,3 +3,18 @@ JavaScript Algorithms and Data Structures Masterclass
 Doubly Linked List
 */
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
