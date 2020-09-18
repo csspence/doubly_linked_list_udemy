0d74bb0e8cc836efd33aab1e57b2432fed251758
Crew Spence <csspence123@gmail.com>
2020-09-18 05:39:16 -0500

    Added push method to doubly linked list.

--- doubly_linked_list_udemy.js
+++ doubly_linked_list_udemy.js
@@ -17,4 +17,18 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push = (val) => {
+    let node = new Node(val);
+    if(this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
 }
