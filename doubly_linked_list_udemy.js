21599516822a85db6e5fc398c90f29b585142037
Crew Spence <csspence123@gmail.com>
2020-09-20 05:03:28 -0500

    Added shift method to doubly linked list.

--- doubly_linked_list_udemy.js
+++ doubly_linked_list_udemy.js
@@ -47,4 +47,20 @@ class DoublyLinkedList {
     this.length--;
     return temp;
   }
+
+  shift = () => {
+    let temp = this.head;
+    if(this.length === 0) {
+      return undefined;
+    } else if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp
+  }
 }
